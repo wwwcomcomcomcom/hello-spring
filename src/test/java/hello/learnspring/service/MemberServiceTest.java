d671d891d06f8eefa07318d4c07d6f6a102a52ae
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-06 21:15:14 +0900

    Test Case for Error Exception

--- src/test/java/hello/learnspring/service/MemberServiceTest.java
+++ src/test/java/hello/learnspring/service/MemberServiceTest.java
@@ -1,13 +1,46 @@
 package hello.learnspring.service;
 
+import hello.learnspring.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
+    MemberService memberService = new MemberService();
+
     @Test
     void join() {
+
+        //given
+        Member member = new Member();
+        member.setName("hello");
+        //when
+        Long saveId = memberService.join(member);
+        //then
+        Member result = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(result.getName());
+
+    }
+
+    @Test
+    public void duplicateMemberException() {
+        //given
+        Member member1 = new Member();
+        member1.setName("hello");
+        Member member2 = new Member();
+        member2.setName("hello");
+        //when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("already exists member");
+        }
+        //then
     }
 
     @Test
