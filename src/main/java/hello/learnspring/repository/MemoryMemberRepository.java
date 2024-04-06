97547b6ea383fc337e1084e05f833cca96f114ca
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-06 22:41:19 +0900

    Component Scan & AutoWired Beans
    Need to learn about [ Spring Bean,
    Dependency Injection, Spring Container ]

--- src/main/java/hello/learnspring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/learnspring/repository/MemoryMemberRepository.java
@@ -1,9 +1,10 @@
 package hello.learnspring.repository;
 
 import hello.learnspring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
-
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static final Map<Long,Member> store = new HashMap<>();
--- src/main/java/hello/learnspring/service/MemberService.java
+++ src/main/java/hello/learnspring/service/MemberService.java
@@ -2,13 +2,16 @@ package hello.learnspring.service;
 
 import hello.learnspring.domain.Member;
 import hello.learnspring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
