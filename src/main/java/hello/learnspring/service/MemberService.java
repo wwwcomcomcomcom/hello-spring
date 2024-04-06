855a461ec77a8ecdc2572bf2ce7ff7fe192cfe9f
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-06 20:35:54 +0900

    Add MemberService

---
+++ src/main/java/hello/learnspring/service/MemberService.java
@@ -0,0 +1,34 @@
+package hello.learnspring.service;
+
+import hello.learnspring.domain.Member;
+import hello.learnspring.repository.MemberRepository;
+import hello.learnspring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        checkDuplicateMember(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void checkDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("already exists member");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
