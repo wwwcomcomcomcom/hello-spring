7f5aff75880cbf6b5b7f60073b4de7a08dd17f95
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-30 21:02:28 +0900

    database connection with jdbc

--- src/main/java/hello/learnspring/SpringConfig.java
+++ src/main/java/hello/learnspring/SpringConfig.java
@@ -1,20 +1,24 @@
 package hello.learnspring;
-
+import hello.learnspring.repository.JdbcMemberRepository;
 import hello.learnspring.repository.MemberRepository;
 import hello.learnspring.repository.MemoryMemberRepository;
 import hello.learnspring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
