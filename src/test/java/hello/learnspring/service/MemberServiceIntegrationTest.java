60b24f82fd7c43a4e3c8e5616e7c4ea15340ffd2
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-05-01 14:52:58 +0900

    test case with spring

--- src/test/java/hello/learnspring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/learnspring/service/MemberServiceIntegrationTest.java
@@ -2,9 +2,6 @@ package hello.learnspring.service;
 
 import hello.learnspring.domain.Member;
 import hello.learnspring.repository.MemberRepository;
-//import hello.learnspring.domain.Member;
-//import hello.learnspring.repository.MemberRepository;
-//import hello.learnspring.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
-@Transactional
+@Transactional //테스트케이스에 이 어노테이션이 있으면 테스트 시작 전에 트랜잭션을 시작하고, 테스트 완료 후에 항상 롤백한다.
 class MemberServiceIntegrationTest {
     @Autowired
     MemberService memberService;
@@ -39,13 +36,8 @@ class MemberServiceIntegrationTest {
         member2.setName("spring");
         //When
         memberService.join(member1);
-        try {
-            memberService.join(member2);
-        } catch (IllegalStateException e) {
-            assertThat(e.getMessage()).isEqualTo("already exists member");
-        }
-//        IllegalStateException e = assertThrows(IllegalStateException.class,
-//                () -> memberService.join(member2));//예외가 발생해야 한다.
-//        assertThat(e.getMessage()).isEqualTo("already exists member");
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        assertThat(e.getMessage()).isEqualTo("already exists member");
     }
 }
